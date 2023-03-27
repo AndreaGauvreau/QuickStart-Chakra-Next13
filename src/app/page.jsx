18ba27f661e7d085ccfb78e345e30a7385a36bc7
Andrea.Gauvreau <andrea@teachizi.fr>
2023-03-27 12:51:21 +0200

    add folders

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+'use client'
+import {Flex, Heading} from '@chakra-ui/react'
+
+export default function Home() {
+  return (
+    <Flex
+      w={'100vw'}
+      h={'100vh'}
+      justifyContent={'center'}
+      alignItems={'center'}
+    >
+      <Heading>Quick Start Next 13 / Chakra ui</Heading>
+    </Flex>
+  )
+}
